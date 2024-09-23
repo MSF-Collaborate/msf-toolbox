ef2b27e35e7f9ad0d8d2917eb5820b03af978ab0
Theo Fromentin <theo.fromentin@amsterdam.msf.org>
2024-09-23 09:24:08 +0200

    Update PowerBI toolbox structure

--- src/msftoolbox/powerbi/power_bi.py
+++ src/msftoolbox/powerbi/power_bi.py
@@ -1,17 +1,13 @@
+
 from typing import Optional
 
 import requests
 import datetime
-import logging
-import json
-from zipfile import ZipFile
-import shutil
-import pandas as pd
+import logging 
 
 log = logging.getLogger()
 
-
-class PowerBI:
+class PowerBIClient:
     """
     A class to interact with the Power BI REST API.
 
@@ -749,96 +745,3 @@ class PowerBI:
         except requests.exceptions.RequestException as e:
             log.error("API call error: {}".format(e))
             return None
-
-
-class ReportExtractor:
-    """
-    A class to extract information from Power BI report files.
-
-    This class provides methods to extract measures, columns, and aggregations from a Power BI report
-    file (with a .pbix extension). It reads the report's internal JSON configuration and extracts
-    relevant information.
-
-    Attributes:
-        path (str): The path where the report files are located.
-        name (str): The name of the report file with .pbix extension.
-        result (list): A list to store the extracted information.
-    """
-
-    def __init__(self, path, name):
-        """
-        Initializes the ReportExtractor class.
-
-        Args:
-            path (str): The path where the report files are located.
-            name (str): The name of the report file with pbix extension.
-        """
-        self.path = path
-        self.name = name
-        self.result = []
-
-    def extract(self):
-        """
-        Extracts information from the report file.
-
-        This function extracts information from a report file in a specific format.
-        The report file is a ZIP archive containing layout and configuration files.
-
-        The function performs the following steps:
-        1. Create a temporary folder based on the report name.
-        2. Extract the contents of the report file to the temporary folder.
-        3. Load the report layout from the extracted files.
-        4. Iterate over the sections of the report layout and extract information from each visual container.
-        5. Store the extracted information in a list of fields.
-        6. Remove the temporary folder.
-
-        Returns:
-            None
-        """
-        path_folder = f"{self.path}/temp_{self.name[:-5]}"
-        try:
-            shutil.rmtree(path_folder)
-        except FileNotFoundError:
-            print(f"folder {path_folder} not present")
-        with ZipFile(f"{self.path}/{self.name}", "r") as f:
-            f.extractall(path_folder)
-
-        with open(f"{path_folder}/Report/Layout", "r", encoding="utf-16 le") as report_file:
-            report_layout = json.loads(
-                report_file.read()
-            )
-
-        fields = []
-        for s in report_layout["sections"]:
-            for vc in s["visualContainers"]:
-                try:
-                    query_dict = json.loads(vc["config"])
-                    for command in query_dict["singleVisual"]["prototypeQuery"]["Select"]:
-                        if "Measure" in command.keys():
-                            # - MEASURES
-                            name = command["Name"].split(".")[1]
-                            table = command["Name"].split(".")[0]
-                            fields.append([s["displayName"], query_dict["name"], table, name, "Measure"])
-
-                        elif "Column" in command.keys():
-                            # COLUMNS
-                            name = command["Name"].split(".")[1]
-                            table = command["Name"].split(".")[0]
-                            fields.append([s["displayName"], query_dict["name"], table, name, "Column"])
-
-                        elif "Aggregation" in command.keys():
-                            # AGGREGATIONS
-                            if ("(" in command["Name"]) & (")" in command["Name"]):
-                                txt_extraction = command["Name"][
-                                    command["Name"].find("(") + 1 : command["Name"].find(")")
-                                ]
-                                name = txt_extraction.split(".")[1]
-                                table = txt_extraction.split(".")[0]
-                                fields.append(
-                                    [s["displayName"], query_dict["name"], table, name, "Aggregation"]
-                                )
-
-                except (KeyError, json.JSONDecodeError):
-                    pass
-        self.result = fields
-        shutil.rmtree(path_folder)
