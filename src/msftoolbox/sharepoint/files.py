b8571cf012b94484f95dc7f1148ad831b00bcb57
Theo Fromentin <theo.fromentin@amsterdam.msf.org>
2024-10-02 13:58:36 +0200

    Update returned metadata in list methods and methods to interact with non existing folders

--- src/msftoolbox/sharepoint/files.py
+++ src/msftoolbox/sharepoint/files.py
@@ -1,7 +1,7 @@
 from typing import List
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.client_context import ClientContext
-from office365.sharepoint.files.file import File
+import os
 
 class SharePointClient:
     """
@@ -46,84 +46,154 @@ class SharePointClient:
 
         Args:
             folder_url (str): The server-relative URL of the folder.
-            keep_metadata (bool): If false returns only the server url, else the full properties object
+            keep_metadata (bool): If false returns only the name and server url, else the full properties object
         Returns:
-            List[str]: A list of file names in the folder.
+            List[dict]: A list of file records in the folder.
         """
         # Get the folder
         folder = self.context.web.get_folder_by_server_relative_url(
             folder_url
             )
 
+        # List the files in the folder
         files = folder.files
         self.context.load(files)
         self.context.execute_query_with_incremental_retry()
+
+        # If the keep_metadata attribute is True then keep
+        # all properties else subset for commonly used ones
         if keep_metadata:
-            return [file.properties for file in files]
+            records = [
+                file.properties for file in files
+                ]
         else:
-            return [file.properties['ServerRelativeUrl'] for file in files]
+            records = [{
+                field: file.properties[field]
+                for field in [
+                    "Name",
+                    "ServerRelativeUrl",
+                    "TimeLastModified"
+                    ]
+                } for file in files]
+        
+        return records
 
 
     def list_folders_in_folder(
         self,
-        folder_url: str
+        folder_url: str,
+        keep_metadata: bool = False
         ) -> List[str]:
         """
         Lists all files in a specified folder.
 
         Args:
             folder_url (str): The server-relative URL of the folder.
+            keep_metadata (bool): If false returns only the name and server url, else the full properties object
 
         Returns:
-            List[str]: A list of file names in the folder.
+            List[dict]: A list of folder records in the folder.
         """
         # Get the folder
         folder = self.context.web.get_folder_by_server_relative_url(
             folder_url
             )
 
+        # List folders in the folder
         folders = folder.folders
         self.context.load(folders)
         self.context.execute_query_with_incremental_retry()
-        return [folder.properties['Name'] for folder in folders]
+
+        # If the keep_metadata attribute is True then keep
+        # all properties else subset for commonly used ones
+        if keep_metadata:
+            records = [
+                folder.properties for folder in folders
+                ]
+        else:
+            records = [{
+                field: folder.properties[field]
+                for field in [
+                    "Name",
+                    "ServerRelativeUrl",
+                    "TimeLastModified"
+                    ]
+                } for folder in folders]
+
+        return records
 
     def download_file(
         self,
-        file_url: str,
-        download_path: str
+        source_url: str,
+        destination_file_path: str
         ) -> None:
         """
         Downloads a file from SharePoint.
 
         Args:
-            file_url (str): The server-relative URL of the file.
-            download_path (str): The local path where the file will be saved.
+            source_url (str): The server-relative URL of the file.
+            local_file_path (str): The local path where the file will be saved.
         """
-        file = self.context.web.get_file_by_server_relative_url(file_url)
+        # Get the file
+        file = self.context.web.get_file_by_server_relative_url(
+            source_url
+            )
+
         self.context.load(file)
         self.context.execute_query_with_incremental_retry()
-        with open(download_path, "wb") as local_file:
-            file.download(local_file).execute_query()
-        print(f"Downloaded: {download_path}")
 
-    def load_files(
+        # Load the file to the local file path
+        with open(destination_file_path, "wb") as local_file:
+            file.download(
+                local_file
+                ).execute_query_with_incremental_retry()
+
+        print(f"Downloaded: {destination_file_path}")
+        return None
+
+    def upload_file(
         self,
-        folder_url: str
-        ) -> List[File]:
+        source_file_path: str,
+        destination_url: str
+        ) -> None:
         """
-        Loads all files in a specified folder.
+        Uploads a file to the sharepoint folder.
+        Files up to 4MB are accepted.
 
         Args:
-            folder_url (str): The server-relative URL of the folder.
-
-        Returns:
-            List[File]: A list of File objects in the folder.
+            source_file_path (str): The local path of the file too upload.
+            destination_url (str): The server-relative URL of the folder destination.
         """
-        folder = self.context.web.get_folder_by_server_relative_url(folder_url)
-        files = folder.files
-        self.context.load(files)
-        self.context.execute_query_with_incremental_retry()
-        return files
+        # Open the file and store the content
+        with open(source_file_path, "rb") as content_file:
+            file_content = content_file.read()
+
+        folder_exists = self.test_folder_existence(
+            destination_url
+            )
+
+        if not folder_exists:
+            raise Exception("The destination folder does not exist")
+        else:
+            # Get the Sharepoint target folder
+            target_folder = self.context.web.get_folder_by_server_relative_url(
+                destination_url
+                )
+            target_folder.get()
+            target_folder.execute_query_with_incremental_retry()
+
+            # Get the file name based on the source name
+            name = os.path.basename(
+                source_file_path
+                )
+
+            # Upload
+            target_folder.upload_file(
+                name,
+                file_content
+                ).execute_query_with_incremental_retry()
+
+            return None
 
     def recursively_list_files(
         self,
@@ -141,18 +211,18 @@ class SharePointClient:
             List[str]: A list of all file names in the folder and its subfolders.
         """
         # Get the folders in the root folder
-        folder = self.context.web.get_folder_by_server_relative_url(folder_url)
-        folders = folder.folders
-        self.context.load(folders)
-        self.context.execute_query_with_incremental_retry()
+        folders = self.list_folders_in_folder(
+            folder_url
+            )
 
         # Get all the files in the folder
         all_files = []
         for subfolder in folders:
-            subfolder_url = subfolder.properties['ServerRelativeUrl']
+            subfolder_url = subfolder["ServerRelativeUrl"]
             all_files.extend(
                 self.recursively_list_files(
-                    subfolder_url
+                    subfolder_url,
+                    keep_metadata
                     )
                 )
 
@@ -177,27 +247,39 @@ class SharePointClient:
         Returns:
             str: The server-relative URL of the starting folder.
         """
-        folder = (
-            self.context.web.ensure_folder_path(folder_url)
-            .get()
-            .select(["ServerRelativePath"])
-            .execute_query()
-        )
-        return folder.server_relative_path
+        folder_test = self.test_folder_existence(folder_url)
 
-    # def execute_query_with_incremental_retry(
-    #     self
-    #     ) -> None:
-    #     """
-    #     Executes a query with throttling handling.
-    #     Retries the request after a delay if throttled.
-    #     """
-    #     try:
-    #         self.context.execute_query()
-    #     except Exception as ex:
-    #         if "429" in str(ex):
-    #             print("Throttled by SharePoint, waiting for 30 seconds...")
-    #             time.sleep(30)
-    #             self.context.execute_query_with_incremental_retry()
-    #         else:
-    #             raise ex
+        if folder_test:
+            print("Folder exits already.")
+        else:
+            print("Creating folder.")
+
+            folder_test = (
+                self.context.web.ensure_folder_path(folder_url)
+                .get()
+                .select(["ServerRelativePath"])
+                .execute_query_with_incremental_retry()
+            )
+        return None
+
+
+    def test_folder_existence(
+        self,
+        folder_url
+        ):
+        """Tests for the existence of a folder at the server-relative URL.
+
+        Args:
+            folder_url (str): The server-relative URL of the starting folder.
+
+        Returns:
+            str: The server-relative URL of the starting folder.
+        """
+        folder_test = (
+            self.context.web.get_folder_by_server_relative_path(folder_url)
+            .get()
+            .execute_query_with_incremental_retry()
+            )
+
+        folder_test = folder_test.properties.get("Exists", False)
+        return folder_test
