13e43a606c850d85257b9c9b4529481d012c2740
Derek Loots <derek.loots@amsterdam.msf.org>
2024-10-18 09:25:22 +0200

    update comments theo

--- src/msftoolbox/gdelt/articles.py
+++ src/msftoolbox/gdelt/articles.py
@@ -7,11 +7,9 @@ class GDELTExtractor:
     def __init__(
         self,
         sort="HybridRel",
-        limit=50,
-        mode="ArtList"
+        limit=50
     ):
         self.sort = sort
-        self.mode = mode
         self.limit = limit
 
     def format_date(self, date) -> str:
@@ -24,18 +22,35 @@ class GDELTExtractor:
         end_date: str,
         query_value: str,
         country_filter: list = None,
-        response_format: str = "JSON"
+        mode: str = "ArtList"
     ) -> list:
+        """
+        Retrieve a list of reports from the GDELT API based on specified criteria.
+
+        Args:
+            start_date (str): The start date for the reports in YYYY-MM-DD format.
+            end_date (str): The end date for the reports in YYYY-MM-DD format.
+            query_value (str): The search query value.
+            country_filter (list, optional): A list of countries to filter the reports. Defaults to None.
+            response_format (str, optional): The format of the response, default is "JSON".
+            mode (str, optional): The mode for the API request, default is "ArtList".
+
+        Returns:
+            list: A list of articles from the response data.
+
+        Raises:
+            HTTPError: If the HTTP request returns a status code other than 200.
+        """
         base_url = "https://api.gdeltproject.org/api/v2/doc/doc"
         start_date = self.format_date(start_date)
         end_date = self.format_date(end_date)
 
         params = {
             "query": f"{query_value} AND {country_filter}",
-            "mode": self.mode,
+            "mode": mode,
             "startdatetime": start_date,
             "enddatetime": end_date,
-            "format": response_format,
+            "format": "JSON",
             "trans": "googtrans"
         }
 
@@ -53,7 +68,7 @@ class GDELTExtractor:
             report_url (str): The url for the report. Typically the href from the list_reports response
 
         Returns:
-            dict: A dictionary containing the full report information or None if an error occurs.
+            dict: Returns the article text containing the full report information or None if an error occurs.
         """
         try:
             page = newspaper.Article(report_url)
